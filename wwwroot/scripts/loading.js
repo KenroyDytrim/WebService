bdf3b293286aa8cc22188b87bea77cd83ede18c4
KenroyDytrim <131690187+KenroyDytrim@users.noreply.github.com>
2024-06-25 18:09:10 +0700

    Third commit

---
+++ wwwroot/scripts/loading.js
@@ -0,0 +1,11 @@
+﻿document.body.onload = function () {
+    setTimeout(function () {
+        var loader = document.getElementById('page-loader');
+        var loader2 = document.getElementById('load-scr');
+        if (!loader.classList.contains('done')) {
+            loader.classList.add('done');
+            loader2.classList.add('done');
+        }
+    }, 5000)
+   
+}
