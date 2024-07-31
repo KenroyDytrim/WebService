887ebea9ee34a8d72fe1b929a33f8a36e893870c
KenroyDytrim <korney1404@gmail.com>
2024-08-01 00:38:42 +0700

    Edit message

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,5 +8,13 @@ namespace Web2.Pages
         public void OnGet()
         {
         }
+         // письмо от пользователя
+        public async Task<IActionResult> OnPostAsync(string name, string email, string phone, string message)
+        {
+            EmailService emailService = new EmailService();
+            await emailService.SendEmailAsync("korney1404@gmail.com", "Письмо от пользователя",
+                $"{message} <div><p>Имя: {name}</p> <p> Email: {email}</p> <p> Телефон: {phone}</p> </div>");
+            return Page();
+        }
     }
 }
